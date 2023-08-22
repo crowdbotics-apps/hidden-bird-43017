31ca38c08ecb4efcc58a2c8eca39acaca41ec8e2
Crowdbotics <team@crowdbotics.com>
2023-08-22 08:38:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hidden-bird-43017",
+  url: "https://hidden-bird-43017.botics.co",
+  api: "https://hidden-bird-43017.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
